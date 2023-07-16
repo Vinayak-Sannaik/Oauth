7fdc40839d9a128fc36533c032c75ec620a75498
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2023-07-16 22:10:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.CALL_BACK_URL,
+}, function (accessToken, refreshToken, profile, done) {
+  console.log(profile);
+  return done(null, profile);
+}));
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: 'failed' }),
+  function (req, res) {
+    res.send('You Successfully log-in Using Google');
+  }
+);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
